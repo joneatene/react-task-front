c303c1f200256f2c0ce8acda84b4df90cfbc9f79
joneatene <joneatene@gmail.com>
2021-07-12 17:10:55 +0300

    Title component and Register page

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,48 +4,64 @@ import * as Yup from "yup";
 import * as S from "./Form.style";
 import Button from "../Button/Button";
 
-const Form = ({type}) => {
+const Form = ({ type }) => {
+  const fetchFunction = (email, password) => {
+    fetch(`https://react-task-back-xtd2r.ondigitalocean.app/${type}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
-    const fetchFunction = (email, password) => {
-        fetch(`https://react-task-back-xtd2r.ondigitalocean.app/${type}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({email, password})
-        }).then(res => res.json()).then(data => console.log(data));
+  const validation = (e) => {
+    e.preventDefault();
+    const email = e.target.elements.email.value.trim();
+    const password = e.target.elements.password.value.trim();
+
+    if (email && password) {
+      const schema = Yup.object().shape({
+        email: Yup.string().email().max(255).min(5).required(),
+        password: Yup.string().max(255).min(8).required(),
+      });
+
+      schema.isValid({ email, password }).then((data) => {
+        data ? fetchFunction(email, password) : alert("Bad email or password");
+      });
+    } else {
+      alert("Please write in email and password");
     }
-    const validation = (e) => {
-        e.preventDefault();
-        const email = e.target.elements.email.value.trim();
-        const password = e.target.elements.password.value.trim();
-
-
-        if(email && password) {
-            const schema = Yup.object().shape({
-                email: Yup.string().email().max(255).min(5).required(),
-                password: Yup.string().max(255).min(8).required(),
-              });
-             
-              schema.isValid({ email, password }).then((data) => {
-                data ? fetchFunction(email, password) : alert("Bad email or password");
-              });
-
-        } else {
-            alert("Please write in email and password")
-        }
-    }
-    return (
-        <form onSubmit={validation}>
-            <S.Input type="email" name="email" minLength="4" maxLength="255" placeholder="Email" required/>
-            <S.Input type="password" name="password" minLength="8" maxLength="255" placeholder="Password" required/>
-            <Button type="submit" color="primary" handleClick="submit">{type}</Button>
-        </form>
-    )
+  };
+  return (
+    <form onSubmit={validation}>
+      <S.Input
+        type="email"
+        name="email"
+        minLength="4"
+        maxLength="255"
+        placeholder="Email"
+        required
+      />
+      <S.Input
+        type="password"
+        name="password"
+        minLength="8"
+        maxLength="255"
+        placeholder="Password"
+        required
+      />
+      <Button type="submit" color="primary" handleClick="submit">
+        {type}
+      </Button>
+    </form>
+  );
 };
 
 Form.propTypes = {
-    type: PropTypes.oneOf(["login", "register"]).isRequired,
+  type: PropTypes.oneOf(["login", "register"]).isRequired,
 };
 
 export default Form;
